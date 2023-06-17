6b664b3c86275fd70c81e363fc91b83b929709e2
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-06-17 17:00:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2356 ms, Memory: 187620 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfPoint = sc.nextInt();
+        ArrayList points = new ArrayList<Point>();
+        for (int i = 0; i < numOfPoint; i++) {
+            int tempX = sc.nextInt();
+            int tempY = sc.nextInt();
+            points.add(new Point(tempX, tempY));
+        }
+        Collections.sort(points);
+        Iterator iterator= points.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Point o) {
+        if (this.y == o.y) {
+           return this.x - o.x;
+        } else {
+            return this.y - o.y;
+        }
+    }
+    @Override
+    public String toString() {
+        return x+" "+y;
+    }
+}
