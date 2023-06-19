7abf2c08474575d7f21276c03eaea31e6ecd6a5a
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-06-20 01:02:09 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 228 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,77 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int m,n;
+
+    m=sc.nextInt();
+    n= sc.nextInt();
+    //저장용 배열
+    boolean[][]arr=new boolean[m][n];
+    //검사해야하는 위치
+    int M=m-7;
+    int N=n-7;
+    int Min=64;
+
+    //배열 입력
+    for(int i=0;i<m;i++){
+        String Input=sc.next();
+        for(int j=0;j<n;j++){
+            if(Input.charAt(j)=='W')
+                arr[i][j]=false;
+            else
+                arr[i][j]=true;
+        }
+    }
+    //배열을 검사
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                int k=Check(arr,i,j);
+                int p=Check2(arr,i,j);
+                if(k<Min)
+                    Min=k;
+                if(p<Min)
+                    Min=p;
+
+            }
+        }
+        System.out.println(Min);
+    }
+    //바꿔야하는 타일의 수를 세는 메서드
+    static int Check(boolean arr[][],int startM,int startN){
+        boolean flag=!arr[startM][startN];
+        int count=0;
+        //검사하기
+        for(int i=startM;i<startM+8;i++){
+            for(int j=startN;j<startN+8;j++){
+                if(flag==arr[i][j]){
+                    count++;
+                    flag=!flag;
+                }
+                else {
+                    flag=arr[i][j];
+                }
+            }
+            flag=!flag;
+        }
+        return count;
+    }
+    static int Check2(boolean arr[][],int startM,int startN){
+        boolean flag=arr[startM][startN];
+        int count=0;
+        //검사하기
+        for(int i=startM;i<startM+8;i++){
+            for(int j=startN;j<startN+8;j++){
+                if(flag==arr[i][j]){
+                    count++;
+                    flag=!flag;
+                }
+                else {
+                    flag=arr[i][j];
+                }
+            }
+            flag=!flag;
+        }
+        return count;
+    }
+}
