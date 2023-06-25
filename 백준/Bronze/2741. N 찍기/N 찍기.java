1c0981b8fcb0e377e6bf009e426e6a2526fe2612
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-06-26 01:08:29 +0900

    [Bronze V] Title: N 찍기, Time: 1072 ms, Memory: 33000 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int k=1;
+        while(true){
+            System.out.println(k);
+            if(k==N)
+                break;
+            k++;
+        }
+    }
+}
